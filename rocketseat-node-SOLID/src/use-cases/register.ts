2e85ba5832cff6d072b93990fc1f8e7c6354bff3
Rafael Medeiros <rafael.medeiros@gertec.com.br>
2025-03-14 13:53:20 -0300

    adiciona tratamento de erros e validação no registro de usuários, incluindo novo repositório Prisma e classe de erro personalizada

--- rocketseat-node-SOLID/src/use-cases/register.ts
+++ rocketseat-node-SOLID/src/use-cases/register.ts
@@ -1,5 +1,6 @@
-import { prisma } from '@/lib/prisma'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { UsersRepository } from '@/repositories/users-repository'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -8,17 +9,13 @@ interface RegisterUseCaseRequest {
 }
 
 export class RegisterUseCase {
-  constructor(private usersRepository: any) {}
+  constructor(private usersRepository: UsersRepository) {}
 
   async execute({ name, email, password }: RegisterUseCaseRequest) {
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw new Error('E-mail already exists.')
+      throw new UserAlreadyExistsError()
     }
 
     const password_hash = await hash(password, 6)
